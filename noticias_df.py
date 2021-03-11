68acb0ad1c932eb66f3c38a39b1815745c5bfe63
J0seca <42120345+J0seca@users.noreply.github.com>
2021-03-11 02:35:39 -0300

    Update noticias_df.py
    Noticias del DF en python

--- noticias_df.py
+++ noticias_df.py
@@ -8,15 +8,9 @@ from bs4 import BeautifulSoup
 import time
 source = urllib.request.urlopen("http://df.cl")
 soup = BeautifulSoup(source)
-#soup.prettify()
-
-#print(soup.find(text="mercado"))
-
-input("...")
 
 for link in soup.find_all("h2"):
-#    print(link)
-    print(link.get_text() + ": \n")
+    print(25*"-" + "\n" + link.get_text() + "\n")
     enlace = str(link.a).split('"')[1:2]
-    print("http://df.cl" + "".join(enlace) + "\n" + 25*"-" + "\n")
-    time.sleep(2)
+    print("http://df.cl" + "".join(enlace) + "\n" + "\n")
+    time.sleep(5)
